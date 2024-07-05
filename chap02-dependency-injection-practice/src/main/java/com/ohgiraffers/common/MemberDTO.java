f76c2344f2bc1de7239f4d3104045d51d22ce40c
[himisterlee] <[lejlej100418@gmail.com]>
2024-07-05 16:44:59 +0900

    chap02 연습문제 완료

---
+++ chap02-dependency-injection-practice/src/main/java/com/ohgiraffers/common/MemberDTO.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.common;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class MemberDTO {
+
+    private Long id;
+    private String nickname;
+}
---
+++ chap02-dependency-injection-practice/src/main/java/com/ohgiraffers/practice02/javaconfig/ContextConfiguration.java
@@ -0,0 +1,36 @@
+package com.ohgiraffers.practice02.javaconfig;
+
+import com.ohgiraffers.common.BoardDTO;
+import com.ohgiraffers.common.MemberDTO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ContextConfiguration {
+
+    @Bean("member")
+    public MemberDTO memberGenerator() {
+
+        MemberDTO member = new MemberDTO();
+
+        member.setId(1L);
+        member.setNickname("봄이올까요");
+
+        return member;
+    }
+
+
+    @Bean("board")
+    public BoardDTO boardGenerator() {
+
+        BoardDTO board = new BoardDTO();
+
+        board.setId(1l);
+        board.setTitle("의존성 주입 연습");
+        board.setContent("게시글의 작성자 객체를 주입 받아볼게요!");
+        board.setWriter(memberGenerator());
+
+        return board;
+    }
+
+}
